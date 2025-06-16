dce4e049b8b429a5fc2abb198fe47aa39a67702f
Kayne Lyu <kangzaiwt@gmail.com>
2025-06-16 19:34:52 +0800

    feat: 添加产品列表

---
+++ app/[locale]/about/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AboutPage() {
+  return (
+    <>
+        <div>关于</div>
+    </>
+  )
+}
--- app/components/Home/product.tsx
+++ app/components/Home/product.tsx
@@ -3,55 +3,62 @@ import { useTranslations } from 'next-intl'
 export default function ProductSection() {
     return (
         <>
-            <div className="bg-black">
+            <div className="bg-black/90">
                 <div>
                     <h1 className="h1-tittle text-white"
                     ><span className="h1-span bg-5">Products</span></h1>
                 </div>
 
                 <ul>
-                    <li>
+                    <li className="px-10 mt-10">
                         <Image
-                            src="/images/solution.png"
+                            src="/images/profile_control.jpg"
                             alt="solution"
                             width={1500}
                             height={1000}
-                            className="m-auto w-full md:w-400 h-auto"
+                            className="m-auto w-full md:w-400 h-auto rounded-lg"
                         />
+                        <p className="font-semibold font-[family-name:var(--font-Public-Sans)] text-white text-center my-5">Profile control</p>
                     </li>
-                    <li>
+                    <li className="px-10 mt-10">
                         <Image
-                            src="/images/solution.png"
+                            src="/images/lift_profile_control.jpg"
                             alt="solution"
                             width={1500}
                             height={1000}
-                            className="m-auto w-full md:w-400 h-auto"
+                            className="m-auto w-full md:w-400 h-auto rounded-lg"
                         />
+                        <p className="font-semibold font-[family-name:var(--font-Public-Sans)] text-white text-center my-5">Lift profile control</p>
                     </li>
-                    <li>
+                    <li className="px-10 mt-10">
                         <Image
-                            src="/images/solution.png"
+                            src="/images/gauge.jpg"
                             alt="solution"
                             width={1500}
                             height={1000}
-                            className="m-auto w-full md:w-400 h-auto"
+                            className="m-auto w-full md:w-400 h-auto rounded-lg"
                         />
-                    </li><li>
+                        <p className="font-semibold font-[family-name:var(--font-Public-Sans)] text-white text-center my-5">Profile measurement</p>
+                    </li>
+                    <li className="px-10 mt-10">
                         <Image
-                            src="/images/solution.png"
+                            src="/images/ring_gauge.jpg"
                             alt="solution"
                             width={1500}
                             height={1000}
-                            className="m-auto w-full md:w-400 h-auto"
+                            className="m-auto w-full md:w-400 h-auto rounded-lg"
                         />
-                    </li><li>
+                        <p className="font-semibold font-[family-name:var(--font-Public-Sans)] text-white text-center my-5">Profile measurement</p>
+                    </li>
+                    <li className="px-10 mt-10">
                         <Image
-                            src="/images/solution.png"
+                            src="/images/dosing.jpg"
                             alt="solution"
                             width={1500}
                             height={1000}
-                            className="m-auto w-full md:w-400 h-auto"
+                            className="m-auto w-full md:w-400 h-auto rounded-lg"
                         />
+                        <p className="font-semibold font-[family-name:var(--font-Public-Sans)] text-white text-center my-5">Dosing</p>
                     </li>
                 </ul>
             </div>
