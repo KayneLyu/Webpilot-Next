fd65890aec258b356d655bc0d772e15c694b0ddf
Kayne Lyu <kangzaiwt@gmail.com>
2025-06-28 18:13:28 +0800

    feat:  渲染markdown文章

--- app/[locale]/news/page.tsx
+++ app/[locale]/news/page.tsx
@@ -1,7 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
+import { getTranslations } from 'next-intl/server';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl'
 
-export default function NewsPage() {
+export default async function NewsPage({ params }: { params: { locale: string } }) {
+  const { locale } = await params;
+  console.log(locale);
+
+  const filePath = path.join(process.cwd(), 'public', 'content', locale, `1.md`);
+  const content = await fs.readFile(filePath, 'utf-8');
+
+  // const metadataPath = path.join(process.cwd(), 'public', 'content', locale, 'metadata.json');
+  // const metadata = JSON.parse(await fs.readFile(metadataPath, 'utf-8'));
   return (
     <>
       <div className='w-full'>
@@ -20,7 +33,20 @@ export default function NewsPage() {
 
         <div className='bg-9 h-500'>
           <div className='max-w-main mx-auto'>
-
+            <div className="prose max-w-none">
+              <ReactMarkdown
+                // components={{
+                //   img({ node, ...props }) {
+                //     const src = props.src?.startsWith('./')
+                //       ? `/content/${locale}/1/${props.src.slice(2)}`
+                //       : props.src;
+                //     return <img {...props} src={src} alt={props.alt} />;
+                //   }
+                // }}
+              >
+                {content}
+              </ReactMarkdown>
+            </div>
           </div>
         </div>
       </div>
