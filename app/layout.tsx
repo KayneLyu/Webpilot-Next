7f13ca7b35d0aba100732a62bee221c8831e4155
Kayne Lyu <kangzaiwt@gmail.com>
2025-07-30 11:07:04 +0800

    Link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 
 import { Roboto_Condensed, Geist_Mono, Public_Sans, Lexend_Exa, Roboto } from "next/font/google";
 import { useLocale } from 'next-intl'
+import Script from "next/script";
 import "./globals.css";
 
 const PublicSans = Public_Sans({
@@ -43,6 +44,22 @@ export default function RootLayout({
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <meta name="theme-color" content="#ffffff" />
                 <script defer src="https://cloud.umami.is/script.js" data-website-id="493f4932-294a-46b6-be6d-e0d345832a41"></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17100334203">
+                </script>
+
+                {/*  Google tag (gtag.js)  */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=AW-17100334203"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-ads" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'AW-17100334203');
+                    `}
+                </Script>
             </head>
             <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
             <body
